a8e88d4908502b1990e7077d20ae60044faaf482
Li Shuanghua <17302618@qq.com>
2024-01-10 22:15:55 +0800

    Format the files of the camera_intrinsic/intrinsic_calib fold

--- camera_intrinsic/intrinsic_calib/include/common.hpp
+++ camera_intrinsic/intrinsic_calib/include/common.hpp
@@ -7,56 +7,56 @@
 
 struct Point2i
 {
-  Point2i() {}
-  Point2i(int x, int y)
-  {
-    this->x = x;
-    this->y = y;
-  }
-  int x;
-  int y;
+    Point2i() {}
+    Point2i(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    int x;
+    int y;
 };
 
 struct Point2f
 {
-  Point2f() {}
-  Point2f(float x, float y)
-  {
-    this->x = x;
-    this->y = y;
-  }
-  float x;
-  float y;
+    Point2f() {}
+    Point2f(float x, float y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    float x;
+    float y;
 };
 
 struct Vector4f
 {
-  Vector4f() {}
-  Vector4f(float x, float y, float z, float w)
-  {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-    this->w = w;
-  }
-  float x;
-  float y;
-  float z;
-  float w;
+    Vector4f() {}
+    Vector4f(float x, float y, float z, float w)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->w = w;
+    }
+    float x;
+    float y;
+    float z;
+    float w;
 };
 
 struct Vector4i
 {
-  Vector4i() {}
-  Vector4i(int x, int y, int z, int w)
-  {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-    this->w = w;
-  }
-  int x;
-  int y;
-  int z;
-  int w;
+    Vector4i() {}
+    Vector4i(int x, int y, int z, int w)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->w = w;
+    }
+    int x;
+    int y;
+    int z;
+    int w;
 };
