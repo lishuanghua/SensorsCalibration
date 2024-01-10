f880010af18418744b3a677889a4a9da0ad0ebe1
Li Shuanghua <17302618@qq.com>
2024-01-10 22:19:42 +0800

    Format the files of the camera_intrinsic/calib_verification fold

--- camera_intrinsic/calib_verification/include/common.hpp
+++ camera_intrinsic/calib_verification/include/common.hpp
@@ -5,50 +5,58 @@
  */
 #pragma once
 
-struct Point2i {
-  Point2i() {}
-  Point2i(int x, int y) {
-    this->x = x;
-    this->y = y;
-  }
-  int x;
-  int y;
+struct Point2i
+{
+    Point2i() {}
+    Point2i(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    int x;
+    int y;
 };
 
-struct Point2f {
-  Point2f() {}
-  Point2f(float x, float y) {
-    this->x = x;
-    this->y = y;
-  }
-  float x;
-  float y;
+struct Point2f
+{
+    Point2f() {}
+    Point2f(float x, float y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    float x;
+    float y;
 };
 
-struct Vector4f {
-  Vector4f() {}
-  Vector4f(float x, float y, float z, float w) {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-    this->w = w;
-  }
-  float x;
-  float y;
-  float z;
-  float w;
+struct Vector4f
+{
+    Vector4f() {}
+    Vector4f(float x, float y, float z, float w)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->w = w;
+    }
+    float x;
+    float y;
+    float z;
+    float w;
 };
 
-struct Vector4i {
-  Vector4i() {}
-  Vector4i(int x, int y, int z, int w) {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-    this->w = w;
-  }
-  int x;
-  int y;
-  int z;
-  int w;
+struct Vector4i
+{
+    Vector4i() {}
+    Vector4i(int x, int y, int z, int w)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->w = w;
+    }
+    int x;
+    int y;
+    int z;
+    int w;
 };
--- camera_intrinsic/calib_verification/src/run_distortion_measure.cpp
+++ camera_intrinsic/calib_verification/src/run_distortion_measure.cpp
@@ -14,18 +14,21 @@ const char usage[] = "\t./bin/run_distortion_measure <distortion_image_path>\n"
                      "example:\n\t"
                      "./bin/run_distortion_measure data/test.png\n";
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    std::cout << argv[0] << usage;
-    return 1;
-  }
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        std::cout << argv[0] << usage;
+        
+        return 1;
+    }
 
-  std::string input_image_path = argv[1];
-  cv::Mat image = cv::imread(input_image_path.c_str(), 0);
+    std::string input_image_path = argv[1];
+    cv::Mat image = cv::imread(input_image_path.c_str(), 0);
 
-  CalibrationHarp distortion_measurement;
-  double d, d_max;
-  auto flag = distortion_measurement.measure(input_image_path, d, d_max);
+    CalibrationHarp distortion_measurement;
+    double d, d_max;
+    auto flag = distortion_measurement.measure(input_image_path, d, d_max);
 
-  return 0;
+    return 0;
 }
