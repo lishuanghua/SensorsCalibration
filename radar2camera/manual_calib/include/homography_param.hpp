e555a21c34d59572fa3fbedfe5fef614b65091a9
Li Shuanghua <17302618@qq.com>
2024-02-15 22:14:05 +0800

    Format the files of the folder radar2camera/manual_calib

--- radar2camera/manual_calib/include/homography_param.hpp
+++ radar2camera/manual_calib/include/homography_param.hpp
@@ -14,41 +14,46 @@
 #include <stdio.h>
 #include <string>
 
-void LoadHomography(const std::string &filename, Eigen::Matrix3d &homo) {
-  Json::Reader reader;
-  Json::Value root;
+void LoadHomography(const std::string &filename, Eigen::Matrix3d &homo)
+{
+    Json::Reader reader;
+    Json::Value root;
 
-  std::ifstream in(filename, std::ios::binary);
+    std::ifstream in(filename, std::ios::binary);
 
-  if (!in.is_open()) {
-    std::cout << "Error Opening " << filename << std::endl;
-    exit(1);
-  }
-  if (reader.parse(in, root, false)) {
-    Json::Value::Members name = root.getMemberNames();
-    std::string id = *(name.begin());
-    std::cout << id << std::endl;
-
-    Json::Value dst = root[id]["param"]["dst_quad"];
-    Json::Value src = root[id]["param"]["src_quad"];
-    if (sizeof(dst) != sizeof(src)) {
-      std::cout << "[ERROR]Dst points num not equal to src points num."
-                << std::endl;
-      exit(1);
+    if (!in.is_open())
+    {
+        std::cout << "Error Opening " << filename << std::endl;
+        exit(1);
     }
-    int num = sizeof(dst);
-    std::vector<cv::Point2f> dst_pts;
-    std::vector<cv::Point2f> src_pts;
-    for (int i = 0; i < num; i++) {
-      cv::Point2f dst_pt(dst[i]["x"].asDouble(), dst[i]["y"].asDouble());
-      cv::Point2f src_pt(src[i]["x"].asDouble(), src[i]["y"].asDouble());
-      dst_pts.push_back(dst_pt);
-      src_pts.push_back(src_pt);
-    }
-    cv::Mat homography = cv::findHomography(src_pts, dst_pts);
-    cv::cv2eigen(homography, homo);
-  }
-  in.close();
+    if (reader.parse(in, root, false))
+    {
+        Json::Value::Members name = root.getMemberNames();
+        std::string id = *(name.begin());
+        std::cout << id << std::endl;
 
-  return;
+        Json::Value dst = root[id]["param"]["dst_quad"];
+        Json::Value src = root[id]["param"]["src_quad"];
+        if (sizeof(dst) != sizeof(src))
+        {
+            std::cout << "[ERROR]Dst points num not equal to src points num." << std::endl;
+
+            exit(1);
+        }
+        int num = sizeof(dst);
+        std::vector<cv::Point2f> dst_pts;
+        std::vector<cv::Point2f> src_pts;
+        for (int i = 0; i < num; i++)
+        {
+            cv::Point2f dst_pt(dst[i]["x"].asDouble(), dst[i]["y"].asDouble());
+            cv::Point2f src_pt(src[i]["x"].asDouble(), src[i]["y"].asDouble());
+            dst_pts.push_back(dst_pt);
+            src_pts.push_back(src_pt);
+        }
+        cv::Mat homography = cv::findHomography(src_pts, dst_pts);
+        cv::cv2eigen(homography, homo);
+    }
+    in.close();
+
+    return;
 }
